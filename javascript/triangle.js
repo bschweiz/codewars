318984eeed14a1dbfeeeba09a4ce91de5ece584c
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-07 15:05:35 -0500

    moving over to python side

--- javascript/triangle.js
+++ javascript/triangle.js
@@ -1,10 +1,9 @@
-function isTriangle(a,b,c)
-{
-  let longestSide = Math.max(a,b,c)
-  let perimeter = a + b + c
-  if ((perimeter - longestSide) > longestSide) {
-    return true
-  } else {
-    return false
-  }
+function isTriangle(a, b, c) {
+    let longestSide = Math.max(a, b, c)
+    let perimeter = a + b + c
+    if ((perimeter - longestSide) > longestSide) {
+        return true
+    } else {
+        return false
+    }
 }
