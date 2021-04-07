55e5cfc742ebbc73f87103d169170ed687d76ba1
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-07 15:03:33 -0500

    more regex examples

--- javascript/regex.js
+++ javascript/regex.js
@@ -1,3 +1,14 @@
 function validatePIN (pin) {
     return /^(\d{4}|\d{6})$/.test(pin);
 }
+
+
+
+function toCamelCase(str){
+
+    var regExp =/[-_]\w/ig;
+
+    return str.replace(regExp,function(match){
+          return match.charAt(1).toUpperCase();
+     });
+}
