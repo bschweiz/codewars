ad7ea0be4236e615aa2f1d43d09e71a887f04486
bschweiz <66841011+bschweiz@users.noreply.github.com>
2021-04-07 22:28:01 -0500

    Update padding.js
    added solutions from answer set

--- javascript/padding.js
+++ javascript/padding.js
@@ -16,3 +16,26 @@ function padIt(str,n){
   return final
   }
 }  
+
+// the nicer solutions:
+
+function padIt(str,n){
+  while(n>0){
+    if(n%2 === 0) {
+      str = str + "*";
+    }else{
+      str = "*" + str;
+    }
+    n --;
+  }
+  return str;
+}
+
+// most elegant
+function padIt(str, n) {
+  while (n > 0) {
+    str = n-- % 2 ? '*' + str : str + '*';
+  }
+  return str;
+}
+
