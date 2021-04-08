09d5e2eb60e018cb7fb6da6d7ab0e6ed5d4ad204
bschweiz <66841011+bschweiz@users.noreply.github.com>
2021-04-08 15:21:54 -0500

    Update ex10.js

--- javascript/ex10.js
+++ javascript/ex10.js
@@ -14,3 +14,16 @@ function pickIt(arr){
   }
   return [odd,even];
 }
+// more elegant:
+
+function pickIt (arr) {
+  
+  let odd = []
+  let even =[]
+      
+  for (var x of arr) {
+      ((x % 2) ? odd : even).push(x)    
+  }
+      
+  return [odd, even]
+}
