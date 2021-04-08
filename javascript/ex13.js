341037008efcfdaec45bc72fa76fbab1cd00402c
bschweiz <66841011+bschweiz@users.noreply.github.com>
2021-04-08 17:01:32 -0500

    Update ex13.js
    need to check answer set for better solutions

--- javascript/ex13.js
+++ javascript/ex13.js
@@ -1,1 +1,20 @@
 // particularities of the number varialbel type
+
+// my solution, issue here is that it doesn't work for negative numbers or zero
+function whatNumberIsIt(n){
+
+  if (n == Number.POSITIVE_INFINITY) {
+      return "Input number is Number.POSITIVE_INFINITY" 
+  } else if (n == Number.MAX_VALUE) {
+    return "Input number is Number.MAX_VALUE" 
+  } else if (n == Number.MIN_VALUE) {
+    return "Input number is Number.MIN_VALUE" 
+  } else if (n == Number.NEGATIVE_INFINITY) {
+    return "Input number is Number.NEGATIVE_INFINITY" 
+  } else if (n != Number && n >1) {
+    console.log(n)
+    var string = "Input number is " + n
+    return string
+} else { return "Input number is Number.NaN"}
+  
+  }
