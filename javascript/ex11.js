bb967751f42db927a381ddf3e7a1e0745bcf3907
bschweiz <66841011+bschweiz@users.noreply.github.com>
2021-04-08 15:44:45 -0500

    Create ex11.js
    solved

---
+++ javascript/ex11.js
@@ -0,0 +1,19 @@
+// breaks and continues in for loops
+// my solution:
+
+function grabDoll(dolls) {
+  
+  var bag=[];
+  
+  for (var i=0; i<dolls.length; i++) {
+   
+      if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
+        bag.push(dolls[i]);
+      } else  {continue;}
+      if (bag.length >= 3) {
+        break;
+      }
+  }
+  return bag;
+  
+}
