e221787a50a1a0fe7b829da5c49963dc02aa5d9f
bschweiz <66841011+bschweiz@users.noreply.github.com>
2021-04-09 13:10:36 -0500

    Update persistentBugger.js
    i like the second one with the .map(Number).reduce very cool combo

--- javascript/persistentBugger.js
+++ javascript/persistentBugger.js
@@ -24,3 +24,26 @@ function persistence(num) {
       } 
     return count;
 }
+
+  
+//    some cool ones:
+  
+function persistence(num) {
+   for (var i = 0; num > 9; i++) {
+     num = num.toString().split('').reduce((t, c) => c * t);
+   }
+   return i;
+}
+  
+function persistence(num) {
+   var times = 0;
+   
+   num = num.toString();
+   
+   while (num.length > 1) {
+     times++;
+     num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+   }
+   
+   return times;
+}
