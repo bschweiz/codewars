a113fd62c323c04dd01d5fbd1b643b33aaac1022
bschweiz <66841011+bschweiz@users.noreply.github.com>
2021-04-12 13:26:31 -0500

    Update jaden_case.py
    don't fully understand the way the import solution work on, need to research further

--- python/jaden_case.py
+++ python/jaden_case.py
@@ -3,3 +3,18 @@
 def to_jaden_case(string):
     
     return " ".join(word.capitalize() for word in string.split())
+
+# different way using an import and a special built-in python func
+
+
+import string
+
+def toJadenCase(NonJadenStrings):
+    return string.capwords(NonJadenStrings)
+
+# or
+
+from string import capwords as toJadenCase
+
+import string
+toJadenCase = string.capwords
