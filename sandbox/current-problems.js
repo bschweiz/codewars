2840c7e75dd3d9bd6594f3acb5d2216aa433276f
Benjamin Schweizer <bschweiz@gmail.com>
2021-05-11 15:03:31 -0500

    added a useful regex file for keeping tricks

--- sandbox/current-problems.js
+++ sandbox/current-problems.js
@@ -26,3 +26,43 @@ waitUntil (args, util) {
 // args needs to be the topic addreass aka heirarchy passed uniquely every 
 // time, not some dumb boolean set and then passed you HDP
     
+
+
+listenToTopicMQTT (args, util) {
+    debugger
+    let stringActions = '';
+
+    if (args === '') {
+        return;
+    }
+
+    // if (args === 'topic') {
+    //     return;
+    // }
+
+    const topic = args.split('/');
+    const last = topic.length - 1;
+    const action = topic[last];
+
+    if (this.actions.length === 0) {
+        const add = {[`${action}`]: false};
+        this.actions.push(add);
+    } else {
+        stringActions = JSON.stringify(this.actions);
+        if (stringActions.includes(action)) {
+            this.actions.length = 0;
+            const add = {[`${action}`]: false};
+            this.actions.push(add);
+        } else {
+            const add = {[`${action}`]: false};
+            this.actions.push(add);
+        }
+    }
+
+    console.log(this.actions, 'actions');
+
+    this.runtime.emit('SET_BROADCAST_LISTENER', {
+        topic: args.TOPIC,
+        action: action
+    });
+}
