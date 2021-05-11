807db43d43b430cb5aa929df96be3210355833da
Benjamin Schweizer <bschweiz@gmail.com>
2021-05-11 11:04:24 -0500

    currently trying to solve how to pass more complex data/ arguments into these blocks as triggers or variables

---
+++ sandbox/current-problems.js
@@ -0,0 +1,22 @@
+waitUntil (args, util) {
+        debugger
+        console.log(args, 'args from waitUntil');
+        const topic = args.TOPIC.split('/');
+        const last = topic.length - 1;
+        const action = topic[last];
+        let condition = '';
+
+        for (let i = 0; i < this.actions.length; i++) {
+            const keys = Object.keys(this.actions[i]);
+            if (action === keys[0]) {
+                condition = Object.values(this.actions[i])[0];
+            }
+        }
+
+        if (!condition) {
+            util.yield();
+        } else {
+            return;
+        }
+    }
+    
